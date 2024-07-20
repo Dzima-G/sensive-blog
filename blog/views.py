d73e23018ec67d26e3d0e2170eea3c91a9c4c021
Dzima-G <dgukalin@gmail.com>
2024-07-20 11:24:00 +0300

    code optimization

--- blog/views.py
+++ blog/views.py
@@ -58,7 +58,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': Count(post.likes),
+        'likes_amount': post.likes.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
