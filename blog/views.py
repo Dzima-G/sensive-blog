3825069b11512e89b638c6dfe4c1e2a4b45fa839
Dzima-G <dgukalin@gmail.com>
2024-08-03 16:41:11 +0300

    code optimization

--- blog/views.py
+++ blog/views.py
@@ -41,7 +41,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = get_object_or_404(Post, slug=slug)
+    post = get_object_or_404(Post.objects.popular(), slug=slug)
     comments = post.comments.select_related('post', 'author')
     serialized_comments = []
     for comment in comments:
@@ -58,7 +58,7 @@ def post_detail(request, slug):
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': post.likes.count(),
+        'likes_amount': post.likes_count,
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
